d2390f03350750df2fdd11134ede79c7297ba475
isman <syifa.isman@doku.com>
2025-01-31 18:44:08 +0700

    update form

---
+++ src/main/java/com/api/contract/management/controller/api/TeamDatatablesController.java
@@ -0,0 +1,30 @@
+package com.api.contract.management.controller.api;
+
+import com.api.contract.management.base.command.ServiceExecutor;
+import com.api.contract.management.dto.datatable.DataTableRequest;
+import com.api.contract.management.dto.datatable.DataTableResults;
+import com.api.contract.management.service.contract.TeamDatatablesService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class TeamDatatablesController {
+
+    private final ServiceExecutor serviceExecutor;
+
+    @GetMapping(value = "/team/datatables")
+    public DataTableResults doExecute(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        log.debug("dataTables: {}", httpServletRequest.getRequestURI());
+        DataTableRequest request = new DataTableRequest(httpServletRequest);
+        return serviceExecutor.execute(TeamDatatablesService.class,request);
+    }
+
+}
